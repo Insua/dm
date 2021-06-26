8b82c5cb3a5fb4ed6f41c30075246fb5f5b52542
insua <insua@sohu.com>
2021-06-26 14:51:05 +0800

    fix(property): add map lock

--- zu.go
+++ zu.go
@@ -8,10 +8,12 @@ package dm
 import (
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type Properties struct {
 	innerProps map[string]string
+	sync.RWMutex
 }
 
 func NewProperties() *Properties {
@@ -39,7 +41,9 @@ func (g *Properties) IsNil() bool {
 }
 
 func (g *Properties) GetString(key, def string) string {
+	g.RLock()
 	v, ok := g.innerProps[strings.ToLower(key)]
+	g.RUnlock()
 
 	if !ok || v == "" {
 		return def
@@ -48,7 +52,9 @@ func (g *Properties) GetString(key, def string) string {
 }
 
 func (g *Properties) GetInt(key string, def int, min int, max int) int {
+	g.RLock()
 	value, ok := g.innerProps[strings.ToLower(key)]
+	g.RUnlock()
 	if !ok || value == "" {
 		return def
 	}
@@ -65,7 +71,9 @@ func (g *Properties) GetInt(key string, def int, min int, max int) int {
 }
 
 func (g *Properties) GetBool(key string, def bool) bool {
+	g.RLock()
 	value, ok := g.innerProps[strings.ToLower(key)]
+	g.RUnlock()
 	if !ok || value == "" {
 		return def
 	}
@@ -77,7 +85,9 @@ func (g *Properties) GetBool(key string, def bool) bool {
 }
 
 func (g *Properties) GetTrimString(key string, def string) string {
+	g.RLock()
 	value, ok := g.innerProps[strings.ToLower(key)]
+	g.RUnlock()
 	if !ok || value == "" {
 		return def
 	} else {
@@ -86,7 +96,9 @@ func (g *Properties) GetTrimString(key string, def string) string {
 }
 
 func (g *Properties) GetStringArray(key string, def []string) []string {
+	g.RLock()
 	value, ok := g.innerProps[strings.ToLower(key)]
+	g.RUnlock()
 	if ok || value != "" {
 		array := strings.Split(value, ",")
 		if len(array) > 0 {
@@ -102,7 +114,9 @@ func (g *Properties) GetStringArray(key string, def []string) []string {
 //}
 
 func (g *Properties) Set(key, value string) {
+	g.Lock()
 	g.innerProps[strings.ToLower(key)] = value
+	g.Unlock()
 }
 
 // 如果p有g没有的键值对,添加进g中
@@ -110,9 +124,11 @@ func (g *Properties) SetDiffProperties(p *Properties) {
 	if p == nil {
 		return
 	}
+	g.Lock()
 	for k, v := range p.innerProps {
 		if _, ok := g.innerProps[strings.ToLower(k)]; !ok {
 			g.innerProps[strings.ToLower(k)] = v
 		}
 	}
+	g.Unlock()
 }
